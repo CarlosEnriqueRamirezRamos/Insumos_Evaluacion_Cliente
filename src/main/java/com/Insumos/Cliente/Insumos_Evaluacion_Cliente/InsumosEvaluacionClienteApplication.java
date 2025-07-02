b4c391d6545013d40508ddedd041db7157a5ec39
alien 4 <alien 4@ALIEN4>
2025-07-01 18:28:30 -0600

    Primer Comit Cliente 01/07/2025

---
+++ src/main/java/com/Insumos/Cliente/Insumos_Evaluacion_Cliente/InsumosEvaluacionClienteApplication.java
@@ -0,0 +1,13 @@
+package com.Insumos.Cliente.Insumos_Evaluacion_Cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsumosEvaluacionClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsumosEvaluacionClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Insumos/Cliente/Insumos_Evaluacion_Cliente/InsumosEvaluacionClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Insumos.Cliente.Insumos_Evaluacion_Cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsumosEvaluacionClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
